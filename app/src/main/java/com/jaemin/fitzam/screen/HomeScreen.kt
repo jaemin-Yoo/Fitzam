7297172a08b57d0507aba21377079b193d6b1b96
jaemin-Yoo <iamjm29@gmail.com>
2025-12-21 12:16:48 +0900

    Feat: 홈 화면 상단바, 캘린더 구현

--- app/src/main/java/com/jaemin/fitzam/screen/HomeScreen.kt
+++ app/src/main/java/com/jaemin/fitzam/screen/HomeScreen.kt
@@ -3,20 +3,34 @@ package com.jaemin.fitzam.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jaemin.fitzam.R
+import com.jaemin.fitzam.ui.common.FitzamBrandTopAppBar
 import com.jaemin.fitzam.ui.common.FitzamCalendar
 import com.jaemin.fitzam.ui.theme.FitzamTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Column {
-        FitzamCalendar(
-            modifier = Modifier.padding(16.dp)
-        )
+    Scaffold(
+        topBar = {
+            FitzamBrandTopAppBar(
+                logoRes = R.drawable.fitzam_logo
+            )
+        }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            FitzamCalendar(
+                modifier = Modifier.padding(
+                    vertical = 8.dp,
+                    horizontal = 16.dp
+                )
+            )
+        }
     }
 }
 
