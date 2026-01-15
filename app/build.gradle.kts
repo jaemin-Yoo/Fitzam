e1b720c20061cc13da11ae3a540148da7c69e6cf
jaemin-Yoo <iamjm29@gmail.com>
2026-01-15 22:09:17 +0900

    Feat: 릴리즈 빌드 R8 코드 난독화/최적화/축소 활성화 및 디버그 빌드 타입 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,14 +17,18 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
