e3daeb856ed00825e86a5d2430abf1d619516a3f
jaemin-Yoo <iamjm29@gmail.com>
2026-02-21 17:40:14 +0900

    Feat: 운동 선택 화면 카테고리 텍스트 스타일 변경

--- app/src/main/java/com/jaemin/fitzam/ui/theme/Type.kt
+++ app/src/main/java/com/jaemin/fitzam/ui/theme/Type.kt
@@ -22,6 +22,13 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
     labelLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
